87856024189036322dd5515a38731edec575c1a1
Vladimir Shatalov <ShatalovVS.90@gmail.com>
2025-01-21 23:31:04 +0300

    Delete NewsPortal directory

--- NewsPortal/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_posts, name='all_posts'),
-    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('category/<int:category_id>/', views.posts_by_category, name='posts_by_category'),
-]
