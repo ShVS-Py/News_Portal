fb5b433013202cd7a4a100990b12429e553ccb50
Vladimir Shatalov <ShatalovVS.90@gmail.com>
2025-03-02 11:02:17 +0300

    Delete NewsPortal directory

--- NewsPortal/news/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.apps import apps  # ✅ Это устранит ошибку
-
-class NewsForm(forms.ModelForm):
-    class Meta:
-        model = apps.get_model('news', 'News')  # ✅ Безопасный импорт модели
-        fields = ['title', 'content', 'author']
